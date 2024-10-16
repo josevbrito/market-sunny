7916c71653395c414a9e29af03cb5b7afba2b6bd
josevbrito <josevictorbritoc@gmail.com>
2024-10-16 17:52:47 -0300

    feat: :monocle_face: Migrations

--- 3. CODE/market-sunny/database/migrations/0001_01_01_000000_create_users_table.php
+++ 3. CODE/market-sunny/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,23 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tb_users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('permission');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create('tb_password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('tb_sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
@@ -42,8 +43,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('tb_users');
+        Schema::dropIfExists('tb_password_reset_tokens');
+        Schema::dropIfExists('tb_sessions');
     }
 };
